baa650f32deff23031a3f3e64216cbf52e47d8fd
isarbela <panzeref@gmail.com>
2023-06-15 08:05:54 -0300

    Initial commit + add splash screen + add home screen with floating button

---
+++ app/src/main/java/com/example/plantasia/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.plantasia
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIMEOUT : Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+           val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIMEOUT)
+
+    }
+
+}
